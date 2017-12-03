37e74dfd581c3a18f04904fc3b42289d8fe6041b
Xneg <xnegxneg@gmail.com>
2017-12-03 14:33:52 +0300

    Second part solution. Even worse.

--- Day03.fs
+++ Day03.fs
@@ -42,10 +42,51 @@ let rec calc position direction l =
 
 let getNthElementPosition n = 
     [0..n-1] |> calc (0, 0) Right
-    
+
 let distance position = abs (fst position) + abs (snd position)
+
+let puzzle_input = 289326
 
 getNthElementPosition 12 |> distance
 getNthElementPosition 1024 |> distance
-getNthElementPosition 289326 |> distance
+getNthElementPosition puzzle_input |> distance
 
+let globalMatrix : int [,] = Array2D.init 20 20 (fun _ _ -> 0)
+
+let geometryToArray position =
+    let arrayPos = (Array2D.length1 globalMatrix / 2 + fst position, Array2D.length2 globalMatrix / 2 + snd position)
+    globalMatrix.[fst arrayPos, snd arrayPos]
+
+let neighbours position =
+    [for i in (fst position) - 1 .. (fst position) + 1 do
+        for j in (snd position) - 1.. (snd position) + 1 do
+            yield (i, j)]
+
+let getCellValue =
+    neighbours >> List.sumBy (geometryToArray)
+
+let setValueToArray position =
+    let arrayPos = (Array2D.length1 globalMatrix / 2 + fst position, Array2D.length2 globalMatrix / 2 + snd position)
+    Array2D.set globalMatrix (fst arrayPos) (snd arrayPos)
+
+let rec fillMatrix position direction l =
+    let cellValue = getCellValue position
+    setValueToArray position cellValue
+    match l with
+    | _ when cellValue > puzzle_input -> cellValue
+    | [_] -> 
+        0
+    | _::tail -> 
+        let newPosition = move position direction
+        let newDirection = getDirection newPosition direction
+        fillMatrix newPosition newDirection tail  
+    | _ -> 
+        0
+
+let fillNthElementPosition n = 
+    setValueToArray (0, 0) 1
+    [0..n-1] |> fillMatrix (0, 0) Right
+
+let result = fillNthElementPosition 70
+
+result;;
