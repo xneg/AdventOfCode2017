c986cc6a149c7ec0e7f4b7ce74f2a820fabbdddd
Xneg <xnegxneg@gmail.com>
2017-12-10 12:51:06 +0300

    Day 7 puzzle solving. Playing with trees and discriminated unions.

--- Day07.fs
+++ Day07.fs
@@ -3,49 +3,86 @@ module Day07
 // problem page
 // http://adventofcode.com/2017/day/7
 
-open Utilities
 open System.Text.RegularExpressions
 open System.Collections.Generic
+open System.IO
 
 let problemFileName = @"Data\07.txt"
 
-let lines = problemFileName |> Helper.processFile 
+let processFile (filePath : string) =
+    seq {
+        use fileReader = new StreamReader(filePath)
 
-type Program = string * int 
+        while not fileReader.EndOfStream do
+            let line = fileReader.ReadLine()
+            yield line
+    };;
+
+let lines = problemFileName |> processFile
+
+type Program = { Name : string; Weight : int } 
 
 type Tree = 
     | Branch of Program * Tree list
     | Leaf of Program
 
-let func (x: string[]) : Program = ("dfdf", 1)
-
 let leafs = lines |> Seq.map (fun x -> x.[3..x.Length - 1])
 
-let diskSpace (s : string) =
-    (int)s.[1..s.Length - 2]
+let toProgram (x : string[]) = {Name = x.[0]; Weight = (int)x.[1] } 
 
-let toProgram (x : string[]) : Program = (x.[0], diskSpace x.[1])
+let programs = new Dictionary<string, Program * string[]>()
 
-let programs = new Dictionary<string, Program>
+lines 
+|> Seq.map 
+    (fun x -> 
+        Regex.Split (x, @"\W") 
+        |> Array.filter (fun x -> x <> "")) 
+|> Seq.iter
+    (fun (a : string[]) ->
+        programs.Add(a.[0], (toProgram a, a.[2..])))        
 
-Seq.iter (fun (x : string[]) -> printf "%s" x.[0]) [[|"fjkfpm"; "(69)"; "->"; "kohxzh,"; "liwvq,"; "eqkio,"; "xvoyybs"|];
-     [|"dsiixv"; "(52)"|]; [|"fhimhm"; "(66)"|]; [|"mdlubuq"; "(73)"|]]
+let rec getTree name =
+    let found, value = programs.TryGetValue name
+    if Array.isEmpty (snd value) 
+    then Leaf(fst value)
+    else 
+    let leafs = (snd value) |> Array.map (getTree) |> Array.toList
+    Branch(fst value, leafs)
+let allNodeNames = programs.Values |> Seq.map (snd) |> Seq.fold (fun s x -> Array.concat [|s; x|]) [||]
+let bottom = programs.Keys |> Seq.find (fun x -> not (Array.contains x allNodeNames))
 
-// let nodes2 = lines |> Seq.map (fun x -> Program(x.[0], 1)) //как так??
-// leafs
+//2nd part
 
-// nodes
+let tree = getTree "cqmvs"
 
-// let (|RegexMatchLeaf|_|) (input : string) =
-//     let result = Regex.Match(input, @"(.*?) \((.*?)\)")
-//     if result.Success then
-//         Some(Leaf(result.Groups.[0].Value, (int)result.Groups.[1].Value))
-//     else
-//         None
+let rec getWeight node =
+    match node with
+    | Branch (program, list) -> List.fold (fun s x -> s + getWeight x) program.Weight list
+    | Leaf(program) -> program.Weight
 
-// let (|RegexMatchBranch|_|) (input : string) =
-//     let result =Regex.Match(input, @"(.*?) \((.*?)\) (->) (.*)")
-//     if result.Success then
-//         Some(Branch(result.Groups.[0].Value, (int)result.Groups.[1].Value, list.Empty))
-//     else
-//         None    
+let findBadNode treeList =
+    let badNode =
+        treeList
+        |> List.distinctBy (getWeight)
+        |> List.tryFind (fun _ -> true)
+
+    let badWeight = getWeight badNode.Value
+    let reference = (List.fold (fun s x -> s + getWeight x) 0 treeList - badWeight) / (List.length treeList - 1)
+    if (reference <> badWeight) then
+        Some(badNode.Value, reference - badWeight)
+    else
+        None    
+
+let rec findBadNodeRec tree diff =
+    match tree with
+    | Branch (program, list) -> 
+        let result = findBadNode list
+        if result.IsSome then 
+            let badNode, newDiff = result.Value
+            printfn "%d" newDiff
+            findBadNodeRec badNode newDiff
+        else
+            program.Weight + diff
+    | Leaf (program) -> program.Weight + diff
+
+let properWeight = findBadNodeRec tree 0;;
